ca1b9270a49e65b6f4fa9d4cb1d79369fba54b63
Krishan Shamod <krishanshamod@gmail.com>
2022-03-21 01:25:23 +0530

    Added API configurations

---
+++ app/src/main/java/com/krishanshamod/simple_android_application/api/UserAPIService.kt
@@ -0,0 +1,27 @@
+package com.krishanshamod.simple_android_application.api
+
+import com.krishanshamod.simple_android_application.model.User
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface UserAPIService {
+    @GET("users")
+    fun getUsers(): Call<List<User>>
+
+    @GET("users/{userId}")
+    fun getUser(@Path("userId") id: String): Call<User>
+
+    companion object {
+        val API_URL = "https://jsonplaceholder.typicode.com"
+        fun create(): UserAPIService {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return retrofit.create(UserAPIService::class.java)
+        }
+    }
+}
