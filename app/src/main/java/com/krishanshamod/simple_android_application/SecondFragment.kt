c2e45630ede708114590110e9b4fcfd02c9ba172
Krishan Shamod <krishanshamod@gmail.com>
2022-03-21 02:17:33 +0530

    Implemented the API get methods to the Application

--- app/src/main/java/com/krishanshamod/simple_android_application/SecondFragment.kt
+++ app/src/main/java/com/krishanshamod/simple_android_application/SecondFragment.kt
@@ -1,18 +1,24 @@
 package com.krishanshamod.simple_android_application
 
 import android.os.Bundle
+import android.provider.CalendarContract
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.krishanshamod.simple_android_application.api.UserAPIService
 import com.krishanshamod.simple_android_application.databinding.FragmentSecondBinding
+import com.krishanshamod.simple_android_application.model.User
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
     private val binding get() = _binding!!
+    private val userAPIService = UserAPIService.create()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +33,21 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.textView.text = FirstFragment.userID.toString()
+        val user = userAPIService.getUser(FirstFragment.userID.toString())
+
+        user.enqueue(object: Callback<User> {
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                val body = response.body()
+
+                body?.let {
+                    binding.textView.text = it.email
+                }
+            }
+
+            override fun onFailure(call: Call<User>, t: Throwable) {
+
+            }
+        })
 
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
